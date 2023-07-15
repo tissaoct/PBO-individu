20fe26228a7726e022a345e0d82a15da66b793ab
tissaoct <139540556+tissaoct@users.noreply.github.com>
2023-07-15 15:37:07 +0700

    Create ListView.java

---
+++ ListView.java
@@ -0,0 +1,17 @@
+package com.example.tissa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+
+
+}
